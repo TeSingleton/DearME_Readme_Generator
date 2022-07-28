508e6e65eada7f675646831ba829b371e1be5c66
Te <smoochbanner@icloud.com>
2022-07-27 22:54:24 -0700

    adding and editing develop folder

---
+++ generateMarkdown.js
@@ -0,0 +1,20 @@
+
+
+
+// If there is no license, return empty string
+function renderLicenseSection(License){
+
+}
+
+function renderTitle(title) {
+    return `# ${data.title}`;
+
+    // data.title
+    // data.description
+    // data.authorName
+
+    // TODO: Create a funtion to generate markdown for README
+    function generateMarkdown(data) {
+        return  `${renderTitle(data.title)}`
+    }
+}
