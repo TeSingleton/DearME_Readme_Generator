0011dfa1b7988134d7ec9de2b0ed2becc4b1be6f
Te <smoochbanner@icloud.com>
2022-07-29 20:47:17 -0700

    adding to js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,5 +1,25 @@
 // If there is no license, return empty string
-function renderLicenseSection(License) {}
+
+// apache License 2.0
+// GNU General Public License v3.0
+// MIT  License
+// BSD 2-clause "Simplified" License
+// BSD 3-clause "New" or "Revised" License
+function renderLicenseSection(License) {
+    if (condition) {
+        
+    }else if (condition) {
+        
+    }else if (condition) {
+        
+    } else if (condition) {
+        
+    } else {
+        
+    }{
+        
+    }
+}
 
 function renderTitle(title) {
   return `# ${data.title}`;
@@ -11,5 +31,55 @@ function renderTitle(title) {
   // TODO: Create a funtion to generate markdown for README
   function generateMarkdown(data) {
     return `${renderTitle(data.title)}`;
-  }
-}
+
+  
+
+
+//     `# <Your-Project-Title>
+
+//     ## Description
+    
+//     ${}
+    
+//     ## Table of Contents (Optional)
+    
+//     If your README is long, add a table of contents to make it easy for users to find what they need.
+    
+//     - [Installation](#installation)
+//     - [Usage](#usage)
+//     - [Credits](#credits)
+//     - [License](#license)
+    
+//     ## Installation
+
+//     ${}
+    
+
+//     ## Usage
+    
+//     ${}
+    
+
+    
+//     ## Credits
+    
+//  ${}
+    
+//     ## License
+    
+//    ${}
+    
+   
+
+//     ## Features
+    
+//     ${}
+    
+//     ## How to Contribute
+    
+//    ${}
+//     ## Tests
+    
+//     ${}
+//   }
+// }
--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 // TODO: Include packages needed for this application
-
 // write file
 const fs = require("fs");
-
-// ask the user for data
+// ask the user for data //file system
 const inquirer = require("inquire");
-// generate the html
-const generateMd = require("./utils");
-
-// (./utilities folder from office hours example)
+// generate the html // inquire  npm package that provides an easy way to capture user input.
+const generateMd = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
+// an array of questions to provide into a call function for inquirer.
 const questions = [
   {
     type: "input",
@@ -80,10 +77,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
+// this function will write the data from the inquirer questions into the readme
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
+  inquirer.prompt(questions).then(function (data) {
+    console.log(data);
+    writeToFile("./"(data));
+    console.log("");
+  });
   // Ask the User questions
   // THEN I want to pass the response to generateMD so we can get the  markdown
 }
